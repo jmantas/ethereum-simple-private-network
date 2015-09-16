9d9aa13cda3e47d44cd7b6401693a435f1615c00
arkeo <arkeo@dapp-dev.hungryadmin.org>
2015-09-16 13:57:34 +0300

    New features, configurations

--- eth-net-init.sh
+++ eth-net-init.sh
@@ -2,11 +2,16 @@
 
 . ./parameters.conf
 
+NODEID=$1
+
 if [ ! -d "${DATAROOT}/${NETID}/${NODEID}" ]; then
 	mkdir -p ${DATAROOT}/${NETID}/${NODEID}
 fi
 
+
+echo "Initializing node $1 with private genesis block"
+
 ${GETH} --genesis ./genesis-private.json --nat none --nodiscover --maxpeers 1 \
 --datadir=${DATAROOT}/${NETID}/${NODEID} --networkid ${NETID} -verbosity 6 \
---port ${PORT} --rpcaddr ${RPCADDRESS} --rpcport ${RPCPORT} \
-console 2> ${DATAROOT}/${NETID}/${NODEID}.log
+--port ${PORT}${NODEID} --rpcaddr ${RPCADDRESS} --rpcport ${RPCPORT}${NODEID} \
+console 2>> ${DATAROOT}/${NETID}/${NODEID}.log
