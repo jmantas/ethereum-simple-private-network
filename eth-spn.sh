c2dfa4bdd535ea297398480cc51e3e4715a4fc2a
jmantas <jmantas@users.noreply.github.com>
2016-08-10 19:51:26 +0300

    Parity stuff

--- eth-spn.sh
+++ eth-spn.sh
@@ -7,7 +7,9 @@ RPCPORT=188
 PPROFPORT=606
 RPCADDRESS=127.0.0.1
 GETH=`which geth`
-
+GETHVERSION=`${GETH} version`
+PARITY=`which parity`
+PARITYVERSION=`${PARITY} --version`
 
 checkDir() {
 
@@ -25,7 +27,7 @@ checkDir() {
     then
 
         echo ""
-        echo "Node does not exists. Need to run: ./eth-spn.sh init $1"
+        echo "Node does not exists. Need to run: ./eth-spn.sh initgeth $1"
         echo ""
     
         exit 1
@@ -43,13 +45,13 @@ checkDir() {
 
 }
 
-initNode() {
+initNodeGeth() {
     
     NODEID=$1
     checkDir ${NODEID} 4init
     
     echo ""
-    echo "Initializing node $1 with private genesis block"
+    echo "Initializing GETH node $1 with private genesis block"
     
     echo ""
     echo "Creating new account"
@@ -62,12 +64,38 @@ initNode() {
     echo ""
     ${GETH} --nat none --nodiscover \
     --datadir=${DATAROOT}/${NODEID} --networkid ${NETID} --verbosity 6 \
-    init ./genesis-private.json 
+    init ./genesis-private-geth.json 
 
 }
 
 
-bootupNode() {
+initNodeParity() {
+
+    export RUST_BACKTRACE=1 
+    NODEID=$1
+    checkDir ${NODEID} 4init
+    
+    echo ""
+    echo "Initializing PARITY node $1 with private genesis block"
+    
+    echo ""
+    echo "Creating new account"
+    echo ""
+    ${PARITY} --db-path=${DATAROOT}/${NODEID} \
+    --network-id ${NETID} account new 
+
+    echo ""
+    echo "Importing genesis block and starting initial node"
+    echo ""
+    ${PARITY} --nat none --no-discovery \
+    --db-path=${DATAROOT}/${NODEID} --network-id ${NETID} \
+    --chain ./genesis-private-parity.json
+
+}
+
+
+
+bootupNodeGeth() {
     NODEID=$1
     checkDir ${NODEID} 4bootup
     
@@ -83,15 +111,23 @@ bootupNode() {
     --shh console 2>> ${DATAROOT}/${NODEID}.log
 }
 
+
+
 case "$1" in
-        init)
-        initNode $2
+        initgeth)
+        initNodeGeth $2
         ;;
-        up)
-        bootupNode $2
+        initparity)
+        initNodeParity $2
+        ;;
+        upgeth)
+        bootupNodeGeth $2
+        ;;
+        upparity)
+        bootupNodeParity $2
         ;;
         *)
-        echo "Usage: $0 [ init | up ] [ < node id > ]"
+        echo "Usage: $0 [ initgeth | upgeth ] [ initparity | upparity ] [ < node id > ]"
 RETVAL=$?
 esac
 exit 0
